d4a7df1a3621bcb3a4e2280a537e11b82c1dd74d
jaehong <jaehongjeong777@gmail.com>
2017-11-22 19:10:57 +0900

    ','

---
+++ bit-fbchatbot/rest-api/openapi.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+const searchNewAddress = (type, searchWord) => {
+    var uri = 'http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd';
+    /* Service Key*/
+    var queryString = '?ServiceKey=' + 'GsN31%2BSeUMF3ak8cQzhKH7uzuRuO2Bo6aToZaQtUDlagK1p%2FcAFo4CpUa8Z91IpnM0XrZR4rDzNb%2Fs9Z7NoRAQ%3D%3D';
+
+    /* dong : 동(읍/면)명 road :도로명[default] post : 우편번호 */
+    queryString += '&searchSe=' + type;
+
+    /* 검색어 */
+    queryString += '&srchwrd=' + encodeURIComponent(searchWord); 
+
+    /* 페이지당 출력될 개수를 지정 */
+    queryString += '&countPerPage=10';
+
+    /* 출력될 페이지 번호 */
+    queryString += '&currentPage=1';
+
+    request({
+        uri: uri + queryString,
+    }, function (error, response, body) {
+        console.log('=> Status', response.statusCode);
+        console.log('=> Headers', JSON.stringify(response.headers));
+        console.log('=> Reponse received', body);
+    });  
+};
+
+searchNewAddress('road', '금화로82번길 17');
+
+/*
+module.exports = {
+    searchNewAddress
+};
+*/
