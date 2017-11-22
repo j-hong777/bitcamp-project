34b71e2f4343ffe9d01453dc357fa7cfbad9760e
jaehong <jaehongjeong777@gmail.com>
2017-11-22 19:36:08 +0900

    ','

--- bit-fbchatbot/rest-api/openapi.js
+++ bit-fbchatbot/rest-api/openapi.js
@@ -1,4 +1,5 @@
 const request = require('request');
+require('dotenv').config({path: '/home/ec2-user/vars/.env'})
 
 const searchNewAddress = (type, searchWord) => {
     var uri = 'http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd';
