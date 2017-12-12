baea8ee4fa546642e0130dbb18e4ecceb363dd8e
jaehong <jaehongjeong777@gmail.com>
2017-12-12 20:46:52 +0900

    '.'

--- bit-fbchatbot/iot-api/aws.js
+++ bit-fbchatbot/iot-api/aws.js
@@ -3,7 +3,7 @@ var awsIot = require('aws-iot-device-sdk');
 //장비목록
 const devices = {};
 
-const device = awsIot.device({
+const dev01 = awsIot.device({
     keyPath: "/home/ec2-user/vars/aws-iot/dev01/dev01.private.key",
     certPath: "/home/ec2-user/vars/aws-iot/dev01/dev01.cert.pem", 
     caPath: "/home/ec2-user/vars/aws-iot/root-CA.crt", 
@@ -12,7 +12,7 @@ const device = awsIot.device({
  });
 
 
- device.on('connect', function() {
+ dev01.on('connect', function() {
    console.log('AWS IoT의 dev01  장비와 연결되었음');
    //연결에 성공하면 연결된 장비를 목록에 추가한다.
    devices['dev01'] = dev01;
