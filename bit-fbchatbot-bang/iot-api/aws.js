a8e4c9e03ed5b8707a7e429fe2475ab4dbc6c64a
j-hong777 <jaehongjeong777@gmail.com>
2018-01-11 12:27:37 +0900

    '.'

--- bit-fbchatbot-bang/iot-api/aws.js
+++ bit-fbchatbot-bang/iot-api/aws.js
@@ -54,15 +54,23 @@ dev01.on('message', function(topic, payload) {
     var dataObj = payload.toString('utf-8')
     var obj = JSON.parse(dataObj)
     console.log('받은 메시지:', obj);
-    var temp = obj.temp
-    var humi = obj.humi
-    var dust = obj.dust
+
+    if (obj.get("sensor").equals("dht")){
+        var temp = obj.temp;
+        var humi = obj.humi;
+
+        global.temp = obj.temp;
+        global.humi = obj.humi;
+    }else if (obj.get("sensor").equals("dust")){
+        var dust = obj.dust;
+        global.dust = obj.dust;
+    }
+  
     console.log(temp);
     console.log(humi);
     console.log(dust);
-    global.temp = obj.temp;
-    global.humi = obj.humi;
-    global.dust = obj.dust;
+    
+ 
     console.log('-------------------------');
 
 });
