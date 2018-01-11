72859db04c492e3d7de07468ebc76f5e9f64a3dc
j-hong777 <jaehongjeong777@gmail.com>
2018-01-11 12:34:58 +0900

    '.'

--- bit-fbchatbot-bang/iot-api/aws.js
+++ bit-fbchatbot-bang/iot-api/aws.js
@@ -61,15 +61,15 @@ dev01.on('message', function(topic, payload) {
 
         global.temp = obj.temp;
         global.humi = obj.humi;
+
+        console.log(global.temp);
+        console.log(global.humi);        
     }else if (obj.get("sensor").equals("dust")){
         var dust = obj.dust;
         global.dust = obj.dust;
+        console.log(global.dust);
     }
-  
-    console.log(temp);
-    console.log(humi);
-    console.log(dust);
-    
+ 
  
     console.log('-------------------------');
 
