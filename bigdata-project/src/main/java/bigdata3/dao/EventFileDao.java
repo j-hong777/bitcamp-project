c46cefcb0f4edd67ccbe2bac3c53dd399cc8751b
j-hong777 <jaehongjeong777@gmail.com>
2018-01-16 10:33:12 +0900

    ','

--- bigdata-project/src/main/java/bigdata3/dao/EventFileDao.java
+++
@@ -1,9 +0,0 @@
-package bigdata3.dao;
-
-import bigdata3.domain.Upload;
-
-public interface EventFileDao {
-  // 작성
-  public void insert(Upload upload);
-  public void delete(int branchNo);
-}
--- bigdata-project/src/main/java/bigdata3/dao/FileDao.java
+++
@@ -1,9 +0,0 @@
-package bigdata3.dao;
-
-import bigdata3.domain.Upload;
-
-public interface FileDao {
-  // 작성
-  public void insert(Upload upload);
-  public void delete(int branchNo);
-}
